02af06105487c2902ca793a97e135ddf21efb256
codeswhite <max3227@gmail.com>
2020-06-12 14:42:40 +0300

    Added vscode files

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,8 @@
 from scapy_utils.scapy_utils import arp_request, arp_response, get_gw, unpack_iface
+
+import unittest
+
+
+class TestScapyUtils(unittest.TestCase):
+    def test_bump(self):
+        pass
